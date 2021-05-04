22d4940bd1e8bc24b354210c4aabacf650f6421c
haedo2346 <haedo2346@hotmail.es>
2021-05-03 23:57:22 -0300

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { InicioComponent } from "./components/inicio/inicio.component";
+import { ArticulosFamiliasComponent } from "./components/articulos-familias/articulos-familias.component";
+import { HttpClientModule } from "@angular/common/http";
+
+import { RouterModule } from "@angular/router";
+import { APP_BASE_HREF } from "@angular/common";
+import { MenuComponent } from "./components/menu/menu.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, InicioComponent],
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    ArticulosFamiliasComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: "", redirectTo: "/inicio", pathMatch: "full" },
+      { path: "inicio", component: InicioComponent },
+      { path: "articulosfamilias", component: ArticulosFamiliasComponent }
+    ])
+  ],
+  providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
